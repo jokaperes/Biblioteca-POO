147b95c1499e0f50bcc9b04b1c95d7c9b8434a45
Uri <129327609+Biguri@users.noreply.github.com>
2024-01-30 22:02:27 -0400

    Update Pessoa.java
    Super classe

--- src/main/java/model/Pessoa.java
+++ src/main/java/model/Pessoa.java
@@ -1,1 +1,36 @@
+package main.java.model
+  
+public abstract class Pessoa {
+    private String nome;
+    private String endereco;
+    private String cpf;
+
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public Pessoa() {
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
 
