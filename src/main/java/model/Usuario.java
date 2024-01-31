c77a127dac3c5655b7f414ce1bf432b9dd33e64c
Uri <129327609+Biguri@users.noreply.github.com>
2024-01-30 22:10:12 -0400

    Herança atualizada
    A classe Usuário herda de funcionário que por sua vez Herda da classe Pessoa

--- src/main/java/model/Usuario.java
+++ src/main/java/model/Usuario.java
@@ -1,27 +1,14 @@
 package main.java.model;
 
-public class Funcionario  extends Pessoa  {
+public class Usuario extends Funcionario{
 
-    private String email;
-    private String telefone;
 
-    public String getTelefone() {
-        return telefone;
-    }
+   public Usuario(){
 
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
+   }
 
-    public String getEmail() {
-        return email;
-    }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
 }
-
     
 
