64ba0cee8a191ca0c6a2db9256c818d9ae228010
jokaperes <jokaperes@gmail.com>
2024-02-01 17:32:26 -0400

    update nas classes e comeco do database

--- src/main/java/dao/Database.java
+++ src/main/java/dao/Database.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class Database {
     private static final String URL = "jdbc:mysql://localhost:3306/biblioteca";
     private static final String USER = "root";
-    private static final String PASSWORD = "Jokavini2502";
+    private static final String PASSWORD = "...";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
